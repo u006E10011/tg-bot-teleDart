16224e38ff7bb8a25654bbb9c80f03d8b354eb43
u006E10011 <cloverry2319@gmail.com>
2025-12-05 00:06:04 +0800

    update log starting bot

--- lib/bot.dart
+++ lib/bot.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:telegram_bot/command/command.dart';
@@ -8,12 +9,12 @@ import 'package:telegram_bot/token.dart';
 Future<void> main() async 
 {
   final token = await Token.importToken();
-  final username = (await Telegram(token).getMe()).username;
-  var teledart = TeleDart(token, Event(username!));
+  final bot = await Telegram(token).getMe();
+  var teledart = TeleDart(token, Event(bot.username!));
 
   teledart.start();
   teledart.setMyShortDescription("The bot is written in Dart", "ru");
-  stdout.write("Starting bot");
+  stdout.write("Starting bot: ${bot.firstName}(${bot.username})");
 
   Command(teledart).initCommand();
 }
